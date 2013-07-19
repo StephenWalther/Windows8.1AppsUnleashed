59511a44e38d80584a98e5e5f3bb1fd6b1ac5872
StephenWalther <stephen.walther@superexpert.com>
2013-07-19 14:22:11 -0700

    Updated Chapter02

---
+++ Chapter02 WinJS Fundamentals/Fundamentals/Fundamentals/scheduler/scheduler.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    "use strict";
+
+    function initialize() {
+        // Alias the Scheduler
+        var Scheduler = WinJS.Utilities.Scheduler;
+
+        // Create the jobs
+        var job1 = Scheduler.schedule(
+            function () {
+                console.log("Hello from job1");
+            },
+            Scheduler.Priority.normal
+        );
+
+        var job2 = Scheduler.schedule(
+            function () {
+                console.log("Hello from job2");
+            },
+            Scheduler.Priority.high
+        );
+
+        console.log( Scheduler.retrieveState() );
+    };
+
+    document.addEventListener("DOMContentLoaded", initialize);
+})();
---
+++ Chapter02 WinJS Fundamentals/Fundamentals/Fundamentals/schedulerYield/schedulerYield.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    "use strict";
+
+    function initialize() {
+        // Alias the Scheduler
+        var Scheduler = WinJS.Utilities.Scheduler;
+
+        // Create low priority job
+        Scheduler.schedule(showQuote, Scheduler.Priority.idle);
+
+        // Handle click event
+        document.getElementById("btn").addEventListener("click", function () {
+            writeMessage("Do Something");
+        });
+    };
+
+    function showQuote(jobInfo) {
+        var allQuotes = [
+            "Obstacles are those frightful things you see when you take your eyes off your goal - Ford",
+            "I have not failed. I've just found 10,000 ways that won't work - Edison",
+            "You can't build a reputation on what you are going to do - Ford"
+        ];
+
+        // Busy loop -- don't every do this!
+        while (true) {
+            // Yield to higher priority job
+            if (jobInfo.shouldYield) {
+                writeMessage("Yielding");
+                jobInfo.setWork(showQuote);
+                break;
+            }
+
+            // Display random quote
+            var quote = allQuotes[random(allQuotes.length)];
+            writeMessage(quote);
+        }
+    }
+
+    function writeMessage(message) {
+        var messages = document.getElementById("messages");
+        messages.innerHTML = "<li>" + message + "</li>" + messages.innerHTML;
+    }
+
+    function random(uppperBound) {
+        return Math.floor(Math.random() * uppperBound);
+    }
+
+    document.addEventListener("DOMContentLoaded", initialize);
+})();
