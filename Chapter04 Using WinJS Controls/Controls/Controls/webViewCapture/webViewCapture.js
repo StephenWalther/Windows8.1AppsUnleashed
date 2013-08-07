eb14b83ed20597b2e139e1fe8118da2745da3373
StephenWalther <stephen.walther@superexpert.com>
2013-08-07 09:34:34 -0700

    Added nav bar samples to Chapter 6

--- Chapter04 Using WinJS Controls/Controls/Controls/webViewCapture/webViewCapture.js
+++ Chapter04 Using WinJS Controls/Controls/Controls/webViewCapture/webViewCapture.js
@@ -37,4 +37,7 @@
     }
 
     document.addEventListener("DOMContentLoaded", initialize);
+
+
 })();
+
--- Chapter06 Menus and Flyouts/Menus/Menus/navBar/navBar.js
+++ Chapter06 Menus and Flyouts/Menus/Menus/navBar/navBar.js
@@ -3,6 +3,13 @@
 
     function initialize() {
         WinJS.UI.processAll().done(function () {
+
+            //var navBar1 = document.getElementById("navBar1");
+            //navBar1.addEventListener("invoked", function (e) {
+            //    var command = ev.detail.navbarCommand;
+            //    console.log("Navigate to " + command.label);
+            //});
+
         });
     }
 
