710843f404cf15fc2da92d8db5c8c9012438b1dd
StephenWalther <stephen.walther@superexpert.com>
2013-08-26 13:08:50 -0700

    Finished Share Source sample for Chapter13

---
+++ Chapter13 Share and Search/ShareTarget/ShareTarget/pages/add/add.js
@@ -0,0 +1,27 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/add/add.html", {
+        ready: function (element, options) {
+            document.getElementById("frmAdd").addEventListener("submit", function (e) {
+                e.preventDefault();
+
+                // Save new note and navigate home               
+                MyApp.notesDataSource.insertAtEnd(null, {
+                    title: document.getElementById("inpTitle").value,
+                    contents: toStaticHTML(document.getElementById("inpContents").innerHTML)
+                }).done(function (newItem) {
+                    WinJS.Navigation.navigate("/pages/home/home.html");
+                });
+            });
+        },
+
+        unload: function () {
+        },
+
+        updateLayout: function (element) {
+        }
+    });
+})();
---
+++ Chapter13 Share and Search/ShareTarget/ShareTarget/pages/home/home.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+
+        ready: function (element, options) {
+            var lvNotes = document.getElementById("lvNotes").winControl;
+
+            // Create data source and bind to ListView
+            lvNotes.itemDataSource = MyApp.notesDataSource;
+        }
+    });
+})();
