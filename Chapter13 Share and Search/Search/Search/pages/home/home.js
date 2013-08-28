d9adc35ddd7b07e0a61df773f35c09c3740f1905
StephenWalther <stephen.walther@superexpert.com>
2013-08-28 12:03:06 -0700

    Added Search sample to Chapter13

---
+++ Chapter13 Share and Search/Search/Search/pages/home/home.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+
+        ready: function (element, options) {
+            var lvNotes = document.getElementById("lvNotes").winControl;
+
+            // Create data source and bind to ListView
+            lvNotes.itemDataSource = MyApp.notesDataSource;
+
+            // Listen for changes to notes
+            Windows.Storage.ApplicationData.current.addEventListener("datachanged", function () {
+                lvNotes.itemDataSource.reload();
+            });
+
+        }
+    });
+})();
